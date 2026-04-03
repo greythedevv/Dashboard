9a87c9e688d71be3fda85390e2283f7e9e4ff807
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-04-03 20:50:20 +0100

    imported tailwind

--- dashboard/src/components/Dashboard.jsx/dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx/dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Dashboard = () => {
   return (
     <div className='dashboard-container'>
-      <div className='dashboard'>Dashboard</div>
+      <div className='dashboard' >Dashboard</div>
     </div>
   )
 }
--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,5 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+   plugins: [tailwindcss()]
 })
