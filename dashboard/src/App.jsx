04ea4b857fa6aa77accc8418468e63642095cb65
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-04-03 15:47:14 +0100

    dashboard

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { Dashboard } from './components/Dashboard.jsx/dashboard'
+import { Sidebar } from './components/sidebar/sidebar'
+
 
 function App() {
   
 
   return (
     <>
-     
+    <div className='app-container'>
+    <Sidebar />
+    <Dashboard />
+     </div>
     </>
   )
 }
---
+++ dashboard/src/components/Dashboard.jsx/dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Dashboard = () => {
+  return (
+    <div className='dashboard-container'>
+      <div className='dashboard'>Dashboard</div>
+    </div>
+  )
+}
+
+
