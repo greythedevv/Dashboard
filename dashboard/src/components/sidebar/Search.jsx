9dcd03f2474d4f94aed6aa52620dac9351c2728a
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-04-04 03:23:17 +0100

    DASHBOARD

--- dashboard/src/components/sidebar/Search.jsx
+++ dashboard/src/components/sidebar/Search.jsx
@@ -1,11 +1,24 @@
+"use client"
+
 import React from 'react'
+import { FiCommand, FiSearch } from 'react-icons/fi'
+import { CommandMenu } from './CommandMenue'
+import { useState } from 'react'
 
 export const Search = () => {
-  return (
-    <div>
-      
+      const [open, setOpen] = useState(false)
+
+  return (<>
+    <div className='bg-stone-200 mb-4 relative rounded flex item-center px-2 py-1.5 text-sm'>
+      <FiSearch className='mr-2'/>
+      <input onFocus={(e)=>{e.target.blur();setOpen(true)}} type="text" placeholder='Search' className='w-full bg-transparent placeholder:text-stone-400 focus:outline-none' />
+      <span className='p-1 text-xs flex gap-0.5 items-center shadow bg-stone-50 rounded absolute right-1.5 top-1/2-translate-y-1/2'>
+      <FiCommand/>
+       K
+      </span>
     </div>
-  )
+    <CommandMenu  open={open} setOpen={setOpen} />
+ </> )
 }
 
 
--- dashboard/src/components/sidebar/sidebar.jsx
+++ dashboard/src/components/sidebar/sidebar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { AccountToggle } from './AccountToggle'
 import { Search } from './Search'
+import {RouteSelect} from './RoutesSelect'
+import { Plan } from './Plan'
 
 export const  Sidebar = () => {
   return (
@@ -8,10 +10,12 @@ export const  Sidebar = () => {
       <div className='overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]'>
             <AccountToggle />
             <Search />
+            <RouteSelect />
 
         {/* to do main side bar content */}
       </div>
       <div>
+       <Plan />
         {/* todo plan tggle */}
       </div>
     </div>
